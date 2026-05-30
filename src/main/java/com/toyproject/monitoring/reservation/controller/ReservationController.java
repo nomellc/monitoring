ee23554ea3be7b57e0f3551f98ba7ace8557ae6f
nomellc <jyj100743@ewha.ac.kr>
2026-05-30 23:48:39 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/com/toyproject/monitoring/reservation/controller/ReservationController.java
@@ -0,0 +1,7 @@
+package com.toyproject.monitoring.reservation.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+}
